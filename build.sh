53d5539bd1f2661241768f8dcc14b89c28772fe5
Calvin Sun <calvin.sun@otms.cn>
2017-04-13 15:03:42 +0800

    Add support for armv7. It seems we still need to support it.

--- build.sh
+++ build.sh
@@ -16,11 +16,16 @@ mkdir -p build-armv7s && cd build-armv7s && gnumake
 
 cd $ICU_ROOT
 
+./configure_armv7.sh
+mkdir -p build-armv7 && cd build-armv7 && gnumake
+
+cd $ICU_ROOT
+
 ./configure_arm64.sh
 mkdir -p build-arm64 && cd build-arm64 && gnumake
 
 cd $ICU_ROOT
 
-echo "Combining x86 64, armv7s, and arm64 libraries."
+echo "Combining x86 64, armv7, armv7s, and arm64 libraries."
 
 ./make_universal.sh
